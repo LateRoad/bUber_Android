66a9103b1d0121a7b3af95e3a491666ed0554cbc
Late Roman <lateroad@gmail.com>
2018-03-23 15:10:59 +0300

    Clean up.

--- app/src/main/java/com/lateroad/buber/activity/TripsActivity.java
+++ app/src/main/java/com/lateroad/buber/activity/TripsActivity.java
@@ -1,9 +1,7 @@
 package com.lateroad.buber.activity;
 
-import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
